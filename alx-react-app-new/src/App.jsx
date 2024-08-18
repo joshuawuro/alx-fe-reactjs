f5718396e8f559a8cd8cfd5fc9a68733dfb23a54
Joshuawuro <joshuawuro@gmail.com>
2024-08-18 15:28:19 -0700

    feat: implement counter in app.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Header />
       <MainContent />
+      <Counter />
       <Footer />
     </>
   );
