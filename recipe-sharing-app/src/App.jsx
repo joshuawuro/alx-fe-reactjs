6a77f8226a0cbf38f7988da2fdf0803bd88a7425
Joshuawuro <joshuawuro@gmail.com>
2024-08-25 21:57:04 -0700

    initial commit

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
+
+function App() {
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  );
+}
+
+export default App;
