c6b36b54234eb3433f89d4297ab125f69d36683f
Joshuawuro <joshuawuro@gmail.com>
2024-08-25 22:23:22 -0700

    feat: integrate react-router-dom for recipe details navigation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
+// src/App.jsx
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Manager</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Recipe Manager</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
