2ebf03f547fb08edb43389e0d83b61c57095aef0
Joshuawuro <joshuawuro@gmail.com>
2024-09-05 01:54:09 -0700

    feat: add interactivity and transition to card, image and header

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 md:max-w-xs sm:max-w-xs">
+    <div className="user-profile bg-gray-100 p-8 md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl sm:p-4 md:p-8 sm:max-w-xs ">
       <img
         src="https://via.placeholder.com/150"
-        className="rounded-full mx-auto md:w-36 md:h-36 sm:w-24 sm:h-24"
+        className="rounded-full mx-auto md:w-36 md:h-36 sm:w-24 sm:h-24 hover:scale-110 transition-transform duration-300 ease-in-out"
         alt="User"
       />
-      <h1 className="text-xl text-blue-800 my-4 md:text-xl sm:text-lg">
+      <h1 className="text-xl text-blue-800 hover:text-blue-500 my-4 md:text-xl sm:text-lg">
         John Doe
       </h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">
