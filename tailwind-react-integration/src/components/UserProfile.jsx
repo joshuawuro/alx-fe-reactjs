64ff4a221beae709ae1c4251bbe5c7d40ed91ef1
Joshuawuro <joshuawuro@gmail.com>
2024-09-05 01:30:08 -0700

    update img to rounded-lg

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-e-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         className="rounded-full w-36 h-36 mx-auto"
