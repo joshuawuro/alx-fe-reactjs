9ac6f82ac8612d012c35d9f57e89694aa1cf39b0
Joshuawuro <joshuawuro@gmail.com>
2024-09-12 20:25:40 -0700

    responsive screen configuration in tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "106px",
+        //=> @media (min-width: 567px){...}
+        tablet: "567px",
+        //=> @media (min-width: 960px) {...}
+        laptop: "992px",
+        //=> @media (min-width: 1024px) {...}
+        desktop: "1200px",
+        //=> @media (min-width: 1440px) {...}
+      },
+    },
   },
   plugins: [],
 };
