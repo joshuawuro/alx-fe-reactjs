fa92bdd4c4ffad5a68ea21e8e4a16a3c6b31b79f
Joshuawuro <joshuawuro@gmail.com>
2024-09-21 01:50:27 -0700

    feat: setup fetchUserData function to get users from github api

---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const GITHUB_API_URL = "https://api.github.com/users/";
+
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default fetchUserData;
