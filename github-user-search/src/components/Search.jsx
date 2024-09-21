4927c15d0ee739a070a0fb93178b64145b7be3bc
Joshuawuro <joshuawuro@gmail.com>
2024-09-21 01:56:34 -0700

    feat: implemtation of loading feature in search component

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -4,6 +4,8 @@ import fetchUserData from "../services/githubService";
 const Search = () => {
   const [username, setUsername] = useState("");
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const handleInputChange = (e) => {
     setUsername(e.target.value);
@@ -11,12 +13,18 @@ const Search = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setError("");
+    setUserData(null);
+
     if (username) {
       try {
         const data = await fetchUserData(username);
         setUserData(data);
       } catch (error) {
-        console.error("Error fetching user data:", error);
+        setError("Looks like we can’t find the user");
+      } finally {
+        setLoading(false);
       }
     }
   };
@@ -32,9 +40,19 @@ const Search = () => {
         />
         <button type="submit">Search</button>
       </form>
+
+      {loading && <p>Loading...</p>}
+
+      {error && <p>{error}</p>}
+
       {userData && (
         <div>
-          <h3>{userData.name}</h3>
+          <img
+            src={userData.avatar_url}
+            alt={`${userData.login}'s avatar`}
+            width="100"
+          />
+          <h3>{userData.name || userData.login}</h3>
           <p>{userData.bio}</p>
           <a href={userData.html_url} target="_blank" rel="noopener noreferrer">
             View Profile
